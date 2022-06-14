8a2430a2339b01f0da59053009f525e8fcc98c20
Wojciech Królik <bylic22@gmail.com>
2022-06-15 00:20:30 +0200

    ✨ Added sign and quizes views.

--- account/urls.py
+++ account/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path("", views.home, name="home"), <-- trzeba zdefiniowac najpierw funkcje wyswietlanai w views.py
+    path("sign", views.sign, name="sign"),
 ]
