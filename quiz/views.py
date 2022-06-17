4bdae33247efab4a05b503bee6adc43d9182acdd
Wojciech Królik <bylic22@gmail.com>
2022-06-17 15:56:47 +0200

    🚧 Form for quiz name and category

--- quiz/views.py
+++ quiz/views.py
@@ -1,10 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import redirect, render
+from .forms import *
+from account.models import User
+
 
 def create(request):
-    return render(request, "quiz/create.html")
+    context = {}
+    if request.method == "POST":
+        form = QuizForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data)
+            quiz = Quiz(
+                name=form.cleaned_data["name"],
+                category=form.cleaned_data["category"],
+                # id_user=User.objects.get(login=request.POST["active"]),
+            )
+            quiz.save()
+            return redirect("add")
+    else:
+        form = QuizForm()
+        context["form"] = form
+    return render(request, "quiz/create.html", context)
+
 
 def add(request):
     return render(request, "quiz/add.html")
+
 
 def quizes(request):
     return render(request, "quiz/quizes.html")
